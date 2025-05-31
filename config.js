00f77a529bf3cd5a9ec56a183c121eed850def1f
ITs-Me48 <juniorguibontzdumornay@gmail.com>
2025-05-30 22:13:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "CRAZY;;;jBIBQLZb#WQWAi-Sye-WFNybXSHW-AwHLIhTvcG8f4yzNFeMuMNs",
 // add your Crazy Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -28,13 +28,13 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/hlfa1a.jpeg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/jma6os.jpeg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "CRAZY-MD-V3",
+BOT_NAME: process.env.BOT_NAME || "JUNIOR-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "CRAZY-MD_V3💫",
+STICKER_NAME: process.env.STICKER_NAME || "JUNIOR-MD💫",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -42,13 +42,13 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "24105730123",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50948181169",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ᴄʀᴀᴢʏ ᴅᴇᴠ",
+OWNER_NAME: process.env.OWNER_NAME || "Urr Dªad",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ ᴛᴇᴄʜ ɪɴᴄ*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/149k8x.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/jma6os.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> 🔰 ʜɪ ᴅᴀʀʟɪɴɢ ᴄʀᴀᴢʏ ᴍᴅ ᴠ3 ɪᴛ\'ᴛ ᴀʟɪᴠᴇ ᴀɴᴅ ʀᴇᴀᴅʏ ᴊᴜsᴛ ᴛʏᴘᴇ ᴍᴇɴᴜ🦋\n\n*ᴅᴇsᴄ:* ᴄʀᴀᴢʏ ᴍᴅ ᴠ3 ᴄʜᴜᴅᴅʏ ʙᴜᴅᴅʏ🌻 ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ғᴏʀ ᴀᴜᴛᴏᴍᴀsᴛ ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ ᴀᴄᴄᴏᴜɴᴛ ᴍᴏʀᴇ ᴀɴᴅ ғᴜɴ ᴄᴍᴅs ᴀᴅᴅᴇᴅ✅\n\n> ᴄʀᴇᴀᴛᴇᴅ ᴡɪᴛʜ 💞 ʙʏ ᴄʀᴀᴢʏ ᴅᴇᴠ",
 // add alive msg here 
@@ -62,17 +62,17 @@ MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "private",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
@@ -82,7 +82,7 @@ ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
 /* ʙʏ ᴄʀᴀᴢʏ ᴅᴇᴠ 
